13b89257e35101027e690aae5800b9937025f64f
Aanjaney Kumar Verma <akuver@Aanjaney.local>
2022-01-19 22:20:53 +0530

    Done on 19/01/2021

---
+++ Graphs/planting.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define MOD 1000000007
+#define MODA 998244353
+#define pb push_back
+#define sortv(v) sort(v.begin(), v.end())
+#define sorta(A, N) sort(A, A + N)
+#define debug(x) cerr << #x << " is " << x;
+#define rep(i, a, N) for (ll i = a; i < N; i++)
+#define f first
+#define s second
+#define uniq(v)                                       \
+    {                                                 \
+        sort(v.begin(), v.end());                     \
+        v.erase(unique(v.begin(), v.end()), v.end()); \
+    }
+#define speed                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+using namespace std;
+
+void solve(ll tcase)
+{
+    ll n, ans = 0;
+    cin >> n;
+    vector<ll> deg(n, 0);
+    rep(i, 0, n - 1)
+    {
+        ll u, v;
+        cin >> u >> v;
+        u--, v--;
+        deg[u]++, deg[v]++;
+    }
+    rep(i, 0, n) ans = max(ans, deg[i] + 1);
+    cout << ans;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("planting.in", "r", stdin);
+    freopen("planting.out", "w", stdout);
+#endif
+    speed;
+    ll t = 1;
+    rep(i, 1, t + 1)
+        solve(i);
+}
